210983cf4a19c3841dffd299e00777e1ae50d2f6
newt0 <py_lambda@yahoo.co.jp>
2020-09-26 17:17:46 +0900

    add: Route Button in Header

--- src/App.js
+++ src/App.js
@@ -3,20 +3,22 @@ import "./App.css";
 import Header from "./Header";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import TinderCards from "./TinderCards";
-
+import SwipeButtons from "./SwipeButtons";
 
 function App() {
   return (
     <div className="app">
       <Router>
-        <Header />
         <Switch>
           <Route path="/chat">
+            <Header backButton="/" />
             <h1>Chat page</h1>
           </Route>
 
           <Route path="/">
+          <Header />
             <TinderCards />
+            <SwipeButtons />
           </Route>
         </Switch>
       </Router>
